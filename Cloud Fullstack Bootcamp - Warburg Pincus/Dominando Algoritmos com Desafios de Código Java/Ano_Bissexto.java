58d80ffa6d03ea718fb3558fc5397fa6d787a82f
MarcosGerminiodeSouza <marcos.germiniodesouza@gmail.com>
2023-04-17 11:32:54 -0300

    Java

---
+++ Cloud Fullstack Bootcamp - Warburg Pincus/Dominando Algoritmos com Desafios de Código Java/Ano_Bissexto.java
@@ -0,0 +1,19 @@
+/*
+import java.util.Scanner;
+
+public class Ano_Bissexto {
+
+    public static void main(String[] args) {
+        Scanner ent = new Scanner(System.in);
+        int ano;
+    
+        ano = ent.nextInt();
+        if (ano % 400 == 0)
+          System.out.println("Bissexto");
+        else if(ano % 4 == 0 && ano % 100 != 0)
+          System.out.println("Bissexto");
+        else
+          System.out.println("Nao Bissexto");
+    }
+}
+*/
---
+++ Cloud Fullstack Bootcamp - Warburg Pincus/Dominando Algoritmos com Desafios de Código Java/Taxa_de_Crescimento.java
@@ -0,0 +1,23 @@
+/*
+import java.util.Scanner;
+
+public class Main {
+  
+  public static void main(String[] args) {
+    
+    Scanner input = new Scanner(System.in);
+    
+    double A = input.nextInt();
+    double B = input.nextInt();
+    int anos = 0;
+    
+    while (A < B) {
+      A += (A * 3 / 100);
+      B += (B * 1.5 / 100);
+      anos++;
+    }
+    System.out.println(anos + " anos");
+    
+  }
+}
+*/
