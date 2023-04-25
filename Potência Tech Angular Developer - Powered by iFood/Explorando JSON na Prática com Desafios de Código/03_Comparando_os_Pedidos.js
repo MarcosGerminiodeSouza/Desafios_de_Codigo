4ae7b853c1d8231f4a3b6e75749989e3c7c733ff
MarcosGerminiodeSouza <marcos.germiniodesouza@gmail.com>
2023-04-25 17:11:10 -0300

    JavaScript

---
+++ Potência Tech Angular Developer - Powered by iFood/Explorando JSON na Prática com Desafios de Código/03_Comparando_os_Pedidos.js
@@ -0,0 +1,18 @@
+// JSON do Cliente 1
+const cliente1 = { 
+    nome: gets(),
+    item: gets(), 
+    preco: parseFloat(gets())
+  };
+  
+  // JSON do Cliente 2
+  const cliente2 = { 
+    nome: gets(), 
+    item: gets(), 
+    preco: parseFloat(gets()) 
+  };
+  // Comparar informações
+  if (cliente1.item == cliente2.item & cliente1.nome == cliente2.nome & cliente1.preco == cliente2.preco)
+    print("Os pedidos são iguais");
+  else
+  print("Os pedidos são diferentes");
